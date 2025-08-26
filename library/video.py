23c733867b2fbb1bf2943aaf71f3b839daf637b8
polymorphicengine <gius_martin@yahoo.de>
2025-08-26 17:18:50 +0200

    set env varibale for external screen

--- library/video.py
+++ library/video.py
@@ -1,6 +1,8 @@
 import subprocess
 import config
+import os
 
 def play_video():
    global proc
-   subprocess.call(["cvlc", config.video_dir+"INTRO_HD.mp4",  "--fullscreen", "--play-and-exit"])
+   os.environ['DISPLAY'] = ':0.0'
+   subprocess.call(["cvlc", config.video_dir+"INTRO.mp4",  "--fullscreen", "--play-and-exit"])
